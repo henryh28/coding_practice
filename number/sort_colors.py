a8de10c8bf4709665f6cc4ece646e6af3528e20e
Henry Hsu <h_hsu817@yahoo.com>
2018-09-07 20:48:24 -0700

    Leetcode - 75. Sort Colors
    https://leetcode.com/problems/sort-colors/description/

---
+++ number/sort_colors.py
@@ -0,0 +1,25 @@
+def sortColors(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    front = 0
+    back = len(nums) - 1
+    check = 0
+    
+    while front < back:
+      if nums[front] == 2:
+        nums[front], nums[back] = nums[back], nums[front]
+        back -= 1
+      elif nums[front] == 1:
+        check = front + 1
+        while check <= back:
+          if nums[check] == 0:
+            nums[front], nums[check] = nums[check], nums[front]
+            break
+          else:
+            check += 1
+        front += 1
+      elif nums[front] == 0:
+        front += 1
+        
