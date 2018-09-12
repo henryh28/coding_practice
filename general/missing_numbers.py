0202e6bd9c0da429bfe2a129d4da93ba9d1d29ee
Henry Hsu <h_hsu817@yahoo.com>
2018-09-11 20:05:39 -0700

    Update missing_numbers.py

--- general/missing_numbers.py
+++ general/missing_numbers.py
@@ -1,17 +1,8 @@
 def missingNumbers(arr, brr):
-  dict_a, dict_b, answer = {}, {}, []
+  copy_a = list(arr)
   
-  for value in arr:
-    dict_a[value] = 1 if value not in dict_a else dict_a[value] + 1
-
-  for value in brr:
-    dict_b[value] = 1 if value not in dict_b else dict_b[value] + 1
-  
-  for key, value in dict_b.items():
-    if key not in dict_a:
-      answer.append(key)
-    elif dict_a[key] != value:
-      answer.append(key)
-  
-  return (sorted(answer))
-  
+  for value in copy_a:
+    arr.remove(value)
+    brr.remove(value)
+    
+  return (sorted(set(brr)))
