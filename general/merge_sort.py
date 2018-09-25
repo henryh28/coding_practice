357478aa9bda34eb6d51e91610fab3cd209534d9
Henry Hsu <h_hsu817@yahoo.com>
2018-09-25 13:48:09 -0700

    Merge Sort
    A non-optimized implementation of merge sort (due to heavy use of .pop and constant creation of new 'result' lists)

--- general/merge_sort.py
+++ general/merge_sort.py
@@ -1,22 +1,16 @@
 def mergesort(array):
-  result = []
-
   if len(array) <= 1:
-    return array
+    return (array)
+
+  result = []
 
   middle = len(array) // 2
   left = mergesort(array[:middle])
   right = mergesort(array[middle:])
 
-  while len(left) > 0 and len(right) > 0:
-    if left[0] < right[0]:
-      result.append(left.pop(0))
-    else:
-      result.append(right.pop(0))
+  while left and right:
+    result.append(left.pop(0) if left [0] < right[0] else right.pop(0))
 
-  if len(left) > 0:
-    result.extend(left)
-  else:
-    result.extend(mergesort(right))
+  result.extend(left if left else mergesort(right))
 
   return (result)
