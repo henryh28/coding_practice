69147b6639d7b104b7cca9b72fd4586251c040ca
Henry Hsu <h_hsu817@yahoo.com>
2018-10-04 12:21:05 -0700

    Hackerrank - Java End-of-file
    https://www.hackerrank.com/challenges/java-end-of-file/problem

---
+++ java/java_end_of_file.java
@@ -0,0 +1,10 @@
+public class Solution {
+  
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      for (int i = 1; input.hasNext(); i++) {
+        System.out.println(i + " " + input.nextLine());
+      } 
+    }
+}
